b6d5f8040ee9c659da2a49adca43960eff76c155
jonaphael <jonaphael4@gmail.com>
2018-02-11 17:57:16 -0200

    Blink the RGB LED
    GPIO register map, Clock gating and Digital Enabled

--- main.c
+++ main.c
@@ -1,16 +1,40 @@
-int counter = 0;  
-
 int main()
-{
-  int *p_int;
-  p_int  = &counter;
+{  
+  /* Clock Gating offset 0x608U
+   * Set bit-5 
+   * 0010 0000  = 0x20
+   */
+  *((unsigned int *) 0x400FE608U) = 0x20U;
   
-  while (*p_int < 20){
-    ++(*p_int);
+  
+  /* Pin Direcrion Register (Digital Output) 
+   * offset 0x400U
+   * Set bit-1, bit-2 and bit-3
+   */
+  *((unsigned int *) 0x40025400U) = 0xEU;
+  
+  /* Digital Enabled
+   * offset 0x51CU
+   * Set bit-1, bit-2 and bit-3
+   */
+  *((unsigned int *) 0x4002551CU) = 0xEU;
+  
+  /* Blink */
+  
+  /* LED_R (RED)*/
+  while(1){
+    *((unsigned int *) 0x400253FCU) = 0x2U;
+    
+    /* DELAY */
+    int counter = 0;
+    while(counter < 1000000)
+        ++counter;
+    
+    *((unsigned int *) 0x400253FCU) = 0x0U;
+    counter = 0;
+    while(counter < 1000000)
+        ++counter;
   }
-  
-  p_int = (int *)0x20000002U;
-  *p_int = 0XDEADBEEF;
   
   return 0;   
 }
