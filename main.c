00233232cbb38ddadbe7d1c65f0824a5dd789622
jonaphael <jonaphael4@gmail.com>
2018-02-13 16:04:55 -0200

    Bit-wise operators
    Improving the reability of the code applying macros that use
    bit-shifts.

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include "lm4f120h5qr.h"
 
+#define LED_RED   (1U << 1)
+#define LED_BLUE  (1U << 2)
+#define LED_GREEN (1U << 3)
+
 /*************************************************************************************
-// Run-Mode Clock Gating Regsiter
+// Run-Mode Clock Gating Register
 #define RCGCGPIO (*((unsigned int *) 0x400FE608U))
 
 #define GPIOF_BASE 0x40025000U // GPIOF BASE_ADDRESS
@@ -11,19 +15,20 @@
 **************************************************************************************/
 int main()
 {  
-  SYSCTL_RCGCGPIO_R = 0x20U; /* Set Clock for GPIOF */
-  GPIO_PORTF_DIR_R = 0xEU; /* Set pins 1,2 and 3 as outputs */
-  GPIO_PORTF_DEN_R= 0xEU;  /* Enable Digital*/
+  SYSCTL_RCGCGPIO_R |= (1U << 5); /* Set Clock for GPIOF */
+  GPIO_PORTF_DIR_R |= (LED_RED | LED_BLUE | LED_GREEN); /* Set pins 1,2 and 3 as outputs 0xEU*/
+  GPIO_PORTF_DEN_R |= (LED_RED | LED_BLUE | LED_GREEN);  /* Enable Digital 0xEU*/
 
-  /* LED_R (RED)*/
+  GPIO_PORTF_DATA_R |= LED_BLUE;
+  
   while(1){
-    GPIO_PORTF_DATA_R = 0x2U;
+    GPIO_PORTF_DATA_R |= LED_RED; /* GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R | LED_RED; */
     
     int counter = 0;
     while(counter < 1000000)
         ++counter;
     
-    GPIO_PORTF_DATA_R = 0x0U;
+    GPIO_PORTF_DATA_R &= ~LED_RED; /* GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R & ~LED_RED; */
     counter = 0;
     while(counter < 1000000)
         ++counter;
