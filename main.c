fb0f68915cb2737f8988cf04abd48aee28221e92
jonaphael <jonaphael4@gmail.com>
2018-02-13 00:08:36 -0200

    Pre-Processor and Volatile
    Leaning how to use #define and what's mean when we declare a variable
    with the volatile keyword.

--- main.c
+++ main.c
@@ -1,36 +1,29 @@
+#include "lm4f120h5qr.h"
+
+/*************************************************************************************
+// Run-Mode Clock Gating Regsiter
+#define RCGCGPIO (*((unsigned int *) 0x400FE608U))
+
+#define GPIOF_BASE 0x40025000U // GPIOF BASE_ADDRESS
+#define GPIOF_DIR  (*((unsigned int *) (GPIOF_BASE + 0x400U))) // GPIOF_PIN_DIRECTION
+#define GPIOF_DEN  (*((unsigned int *) (GPIOF_BASE + 0x51CU))) // GPIOF DIGITAL_ENABLE
+#define GPIOF_DATA (*((unsigned int *) (GPIOF_BASE + 0x3FCU))) // GPIOF DATA REGISTER
+**************************************************************************************/
 int main()
 {  
-  /* Clock Gating offset 0x608U
-   * Set bit-5 
-   * 0010 0000  = 0x20
-   */
-  *((unsigned int *) 0x400FE608U) = 0x20U;
-  
-  
-  /* Pin Direcrion Register (Digital Output) 
-   * offset 0x400U
-   * Set bit-1, bit-2 and bit-3
-   */
-  *((unsigned int *) 0x40025400U) = 0xEU;
-  
-  /* Digital Enabled
-   * offset 0x51CU
-   * Set bit-1, bit-2 and bit-3
-   */
-  *((unsigned int *) 0x4002551CU) = 0xEU;
-  
-  /* Blink */
-  
+  SYSCTL_RCGCGPIO_R = 0x20U; /* Set Clock for GPIOF */
+  GPIO_PORTF_DIR_R = 0xEU; /* Set pins 1,2 and 3 as outputs */
+  GPIO_PORTF_DEN_R= 0xEU;  /* Enable Digital*/
+
   /* LED_R (RED)*/
   while(1){
-    *((unsigned int *) 0x400253FCU) = 0x2U;
+    GPIO_PORTF_DATA_R = 0x2U;
     
-    /* DELAY */
     int counter = 0;
     while(counter < 1000000)
         ++counter;
     
-    *((unsigned int *) 0x400253FCU) = 0x0U;
+    GPIO_PORTF_DATA_R = 0x0U;
     counter = 0;
     while(counter < 1000000)
         ++counter;
