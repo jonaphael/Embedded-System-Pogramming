c141e8b30e53b56c82d266930fee46b2755d3030
jonaphael <jonaphael4@gmail.com>
2018-02-16 00:06:03 -0200

    Arrays and Pointer Arithmetic
    switching to GPIO AHB and changing the Read-Modify-Write sequence
    to a simple single write sequence to blink the RGB LED, to avoid ISR
    problems

--- main.c
+++ main.c
@@ -16,19 +16,23 @@
 int main()
 {  
   SYSCTL_RCGCGPIO_R |= (1U << 5); /* Set Clock for GPIOF */
-  GPIO_PORTF_DIR_R |= (LED_RED | LED_BLUE | LED_GREEN); /* Set pins 1,2 and 3 as outputs 0xEU*/
-  GPIO_PORTF_DEN_R |= (LED_RED | LED_BLUE | LED_GREEN);  /* Enable Digital 0xEU*/
+  SYSCTL_GPIOHBCTL_R |= (1U << 5); /* Enable AHB for GPIOF */
+  GPIO_PORTF_AHB_DIR_R |= (LED_RED | LED_BLUE | LED_GREEN); /* Set pins 1,2 and 3 as outputs 0xEU*/
+  GPIO_PORTF_AHB_DEN_R |= (LED_RED | LED_BLUE | LED_GREEN);  /* Enable Digital 0xEU*/
 
-  GPIO_PORTF_DATA_R |= LED_BLUE;
+  GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE] = LED_BLUE; 
   
   while(1){
-    GPIO_PORTF_DATA_R |= LED_RED; /* GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R | LED_RED; */
+    // GPIO_PORTF_DATA_R |= LED_RED; /* GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R | LED_RED; -- read-modify-write sequence */
+    // *((unsigned long volatile *)(0x40025000U + (LED_RED << 2))) = LED_RED; 
+    //  *(GPIO_PORTF_DATA_BITS_R + LED_RED) = LED_RED;
+    GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = LED_RED; 
     
     int counter = 0;
     while(counter < 1000000)
         ++counter;
     
-    GPIO_PORTF_DATA_R &= ~LED_RED; /* GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R & ~LED_RED; */
+    GPIO_PORTF_AHB_DATA_BITS_R[LED_RED]= 0; /* GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R & ~LED_RED; */
     counter = 0;
     while(counter < 1000000)
         ++counter;
