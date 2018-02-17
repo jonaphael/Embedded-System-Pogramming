99598121a53235c14daba84c0cdb195284416c46
jonaphael <jonaphael4@gmail.com>
2018-02-17 19:21:59 -0200

    Function and Stack
    The 2 purposes the statck is used for.
    The BL instruction
    When the main is a LEAF function or not

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #define LED_RED   (1U << 1)
 #define LED_BLUE  (1U << 2)
 #define LED_GREEN (1U << 3)
+#define DELAY_VALUE (1000000)
 
 /*************************************************************************************
 // Run-Mode Clock Gating Register
@@ -13,6 +14,15 @@
 #define GPIOF_DEN  (*((unsigned int *) (GPIOF_BASE + 0x51CU))) // GPIOF DIGITAL_ENABLE
 #define GPIOF_DATA (*((unsigned int *) (GPIOF_BASE + 0x3FCU))) // GPIOF DATA REGISTER
 **************************************************************************************/
+void delay(int iter);
+
+void delay(int iter)
+{
+  int volatile counter = 0;
+  while(counter < iter)
+    ++counter;
+}
+
 int main()
 {  
   SYSCTL_RCGCGPIO_R |= (1U << 5); /* Set Clock for GPIOF */
@@ -20,22 +30,26 @@ int main()
   GPIO_PORTF_AHB_DIR_R |= (LED_RED | LED_BLUE | LED_GREEN); /* Set pins 1,2 and 3 as outputs 0xEU*/
   GPIO_PORTF_AHB_DEN_R |= (LED_RED | LED_BLUE | LED_GREEN);  /* Enable Digital 0xEU*/
 
-  GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE] = LED_BLUE; 
+  
   
   while(1){
-    // GPIO_PORTF_DATA_R |= LED_RED; /* GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R | LED_RED; -- read-modify-write sequence */
-    // *((unsigned long volatile *)(0x40025000U + (LED_RED << 2))) = LED_RED; 
-    //  *(GPIO_PORTF_DATA_BITS_R + LED_RED) = LED_RED;
+    GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE] = LED_BLUE; 
+    delay(DELAY_VALUE);
+    
+    GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE] = 0; 
+    delay(DELAY_VALUE);
+    
     GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = LED_RED; 
+    delay(DELAY_VALUE);
     
-    int counter = 0;
-    while(counter < 1000000)
-        ++counter;
+    GPIO_PORTF_AHB_DATA_BITS_R[LED_RED]= 0;
+    delay(DELAY_VALUE);
     
-    GPIO_PORTF_AHB_DATA_BITS_R[LED_RED]= 0; /* GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R & ~LED_RED; */
-    counter = 0;
-    while(counter < 1000000)
-        ++counter;
+    GPIO_PORTF_AHB_DATA_BITS_R[LED_GREEN]= LED_GREEN;
+    delay(DELAY_VALUE);
+    
+    GPIO_PORTF_AHB_DATA_BITS_R[LED_GREEN]= 0;
+    delay(DELAY_VALUE);
   }
   
   return 0;   
