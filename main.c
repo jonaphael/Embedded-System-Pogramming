668cd4b63fa86e0fece4e9965ac3018ebad1fb70
jonaphael <jonaphael4@gmail.com>
2018-06-30 17:04:56 -0300

    stdint and mixing types
    the special ARM special instructions to read and write
    Bytes, half-words and words.
    Endianess and conversions types

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include "lm4f120h5qr.h"
 #include "delay.h"
 
@@ -16,39 +17,48 @@
 #define GPIOF_DATA (*((unsigned int *) (GPIOF_BASE + 0x3FCU))) // GPIOF DATA REGISTER
 **************************************************************************************/
 
-int *swap(int *x, int *y);
+uint8_t  u8a, u8b;
+uint16_t u16c, u16d;
+uint32_t u32e, u32f;
 
-int *swap(int *x, int *y)
-{
-  static int temp[2]; 
-  
-  temp[0] = *x;
-  temp[1] = *y;
-  
-  *x = temp[1];
-  *y = temp[0];
-  
-  return temp;
-}
+int8_t  s8;
+int16_t s16;
+int32_t s32;
 
 int main()
 { 
+  
+  u8a  = sizeof(u8a);
+  u16c = sizeof(uint16_t);
+  u32e = sizeof(uint32_t);
+  
+  u8a  = 0xa1U;
+  u16c = 0xc1c2U;
+  u32e = 0xe1e2e3e4;
+  
+  u8b  = u8a;
+  u16d = u16c;
+  u32f = u32e;
+  
+  u16c = 40000U;
+  u16d = 30000U;
+  u32e = (uint32_t) u16c + u16d;
+  
+  u16c = 100U;
+  s32  = 10 - (int16_t)u16c;  
+    
   SYSCTL_RCGCGPIO_R |= (1U << 5); /* Set Clock for GPIOF */
   SYSCTL_GPIOHBCTL_R |= (1U << 5); /* Enable AHB for GPIOF */
   GPIO_PORTF_AHB_DIR_R |= (LED_RED | LED_BLUE | LED_GREEN); /* Set pins 1,2 and 3 as outputs 0xEU*/
   GPIO_PORTF_AHB_DEN_R |= (LED_RED | LED_BLUE | LED_GREEN);  /* Enable Digital 0xEU*/
   
   while(1){
-    int x = 1000000;
-    int y = 1000000/2;
-    int *p = swap(&x,&y);
-    
     
     GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE] = LED_BLUE; 
-    delay(p[0]);
+    delay(5000);
     
     GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE] = 0; 
-    delay(p[1]);
+    delay(5000);
     /*
     GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = LED_RED; 
     delay(x);
